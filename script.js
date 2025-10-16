3e251537c84c179ed889ab0c6f6cb87749aecba5
Humberto Barbosa <humbertobarbosa199g@gmail.com>
2025-10-16 13:36:24 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Máscaras simples de input
+document.addEventListener("input", function (e) {
+  if (e.target.id === "cpf") {
+    e.target.value = e.target.value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  }
+
+  if (e.target.id === "telefone") {
+    e.target.value = e.target.value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "($1) $2")
+      .replace(/(\d{5})(\d{4})$/, "$1-$2");
+  }
+
+  if (e.target.id === "cep") {
+    e.target.value = e.target.value
+      .replace(/\D/g, "")
+      .replace(/(\d{5})(\d{3})$/, "$1-$2");
+  }
+});
